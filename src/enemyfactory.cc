b79a2ec525d2239f0ff067a69394bd32d8661ea7
Steve <steve_weng@hotmail.ca>
2014-03-30 02:41:31 -0400

    updated enemyfactory class

--- src/enemyfactory.cc
+++ src/enemyfactory.cc
@@ -1,12 +1,27 @@
 #include "enemyfactory.h"
 #include "vampire.h"
 
-EnemyFactory::EnemyFactory(){}
+EnemyFactory::EnemyFactory() {}
 
-Enemy* EnemyFactory::getEnemy(char enemyChar){
+Enemy *EnemyFactory::getEnemy(char enemyChar)
+{
 
-	Enemy *newEnemy;
-	if (enemyChar == 'V')
-		newEnemy = new Vampire(0,0,Character::generateId());
-	return newEnemy;
+    Enemy *newEnemy;
+    
+    if (enemyChar == 'V')
+        newEnemy = new Vampire(0, 0, Character::generateId());
+    else if (enemyChar == 'W')
+        newEnemy = new Werewolf(0, 0, Character::generateId());
+    else if (enemyChar == 'T')
+        newEnemy = new Troll(0, 0, Character::generateId());
+    else if (enemyChar == 'G')
+        newEnemy = new Goblin(0, 0, Character::generateId());
+    else if (enemyChar == 'M')
+        newEnemy = new Merchant(0, 0, Character::generateId());
+    else if (enemyChar == 'D')
+        newEnemy = new Dragon(0, 0, Character::generateId());
+    else // if (enemyChar == 'P')
+        newEnemy = new Phoenix(0, 0, Character::generateId());
+
+    return newEnemy;
 }
