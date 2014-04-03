bf3d4e8bd2d13552d336699f218ddb3077ad2d3e
Steve <steve_weng@hotmail.ca>
2014-04-03 02:40:47 -0400

    changed some public to private...not many

--- src/characters/dragon.h
+++ src/characters/dragon.h
@@ -8,13 +8,15 @@ class Visitor;
 class Dragon: public Enemy{
 
     Gold *guardedHoard;
+
 public:
     Dragon(int x, int y, int id);
-    ~Dragon();
-    void guard(Gold *hoard);
+    void guard(Gold * hoard);
     void freeHoard();
-    int accept(Visitor &v);
     int getHoardXPos();
     int getHoardYPos();
+    int accept(Visitor &v);
+    ~Dragon();
+
 };
 #endif
--- src/characters/enemy.h
+++ src/characters/enemy.h
@@ -6,21 +6,21 @@ class EnemyInterpreter;
 class Visitor;
 
 class Enemy: public Character{
-
+    void takeDamage(int damage); 
 protected:
     EnemyInterpreter* input;
     char type;
     bool hostile;
     bool mobile;
     Enemy(int hp, int atk, int def, int x, int y, char type, int id,
-        EnemyInterpreter* input, bool hostile = true, bool mobile = true);
+    EnemyInterpreter * input, bool hostile = true, bool mobile = true);
 public:
+    char getSymbol();
+    bool isMobile();
+    int accept(Visitor & v);
     virtual ~Enemy();
     virtual bool isHostile();
     virtual void update();
-    void takeDamage(int damage); 
-    char getSymbol();
-    bool isMobile();
-    int accept(Visitor &v);
+
 };
 #endif
